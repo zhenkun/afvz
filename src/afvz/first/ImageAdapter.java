75bf930e3dc7818174ef1982b8c9a1d4aed029b7
zhenkun <zhenkun@T410.(none)>
2011-10-19 13:59:27 -0700

    updated UI for game1 try again

--- src/afvz/first/ImageAdapter.java
+++ src/afvz/first/ImageAdapter.java
@@ -31,9 +31,9 @@ public class ImageAdapter extends BaseAdapter {
         ImageView imageView;
         if (convertView == null) {  // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+            imageView.setLayoutParams(new GridView.LayoutParams(96, 96));
+//            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//            imageView.setPadding(8, 8, 8, 8);
         } else {
             imageView = (ImageView) convertView;
         }
