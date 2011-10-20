4657ca78543aa5416ae9b6c9ad44002302dc93ef
andrewedwards64 <andrew.edwards64@gmail.com>
2011-10-19 18:54:12 -0700

    added class for board configuration

---
+++ src/afvz/first/numType.java
@@ -0,0 +1,36 @@
+package afvz.first;
+
+
+public class numType {
+
+	public static final int ZERO = 0;
+	public static final int ONE = 1;
+	public static final int TWO = 2;
+	public static final int THREE = 3;
+	public static final int FOUR = 4;
+	public static final int FIVE = 5;
+	public static final int SIX = 6;
+	public static final int SEVEN = 7;
+	public static final int EIGHT = 8;
+	public static final int NINE = 9;
+	public static final int TEN = 10;
+	public static final int ELEVEN = 11;
+	public static final int TWELVE = 12;
+	public static final int THIRTEEN= 13;
+	public static final int FOURTEEN = 14;
+	public static final int FIFTEEN = 15;
+	public static final int SIXTEEN = 16;
+	public static final int SEVENTEEN = 17;
+	public static final int EIGHTEEN = 18;
+	public static final int NINETEEN = 19;
+	public static final int TWENTY = 20;
+	public static final int TWENTYONE = 21;
+	public static final int TWENTYTWO = 22;
+	public static final int TWENTYTHREE = 23;
+	public static final int TWENTYFOUR = 24;
+	public static final int PLUS = 25;
+	public static final int MINUS = 26;
+	public static final int MULTIPLY = 27;
+	public static final int EQUALSIGN = 28;
+	public static final int BLANKTILE = 29;
+}
